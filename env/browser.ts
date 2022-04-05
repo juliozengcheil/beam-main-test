c97afe7ca0f1d80bcb4775f54ef26c384db64b67
julio-zeng-cheil <julio.zeng@cheil.com>
2022-04-05 13:00:58 -0500

    add supabase env to envsafe

--- env/browser.ts
+++ env/browser.ts
@@ -2,7 +2,7 @@ import { invalidEnvError, makeValidator, str, url } from 'envsafe'
 import { bool, envsafe } from 'envsafe'
 
 const supabaseParser = makeValidator<string>((input) => {
-  if (browserEnv.NEXT_PUBLIC_ENABLE_IMAGE_UPLOAD && input === '') {
+  if (process.env.NEXT_PUBLIC_ENABLE_IMAGE_UPLOAD && input === '') {
     throw invalidEnvError('supabase config', input)
   }
   return input
@@ -14,9 +14,12 @@ export const browserEnv = envsafe({
     default: false,
   }),
   // supabase should be in api but for faster dev we useit here on brower
-  NEXT_PUBLIC_SUPABASE_URL: supabaseParser({ allowEmpty: true, default: '' }),
+  NEXT_PUBLIC_SUPABASE_URL: supabaseParser({
+    allowEmpty: true,
+    default: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  }),
   NEXT_PUBLIC_SUPABASE_ANON_KEY: supabaseParser({
     allowEmpty: true,
-    default: '',
+    default: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   }),
 })
