a8832dff9993d24da0c8744e381907f1155e47ad
julio-zeng-cheil <julio.zeng@cheil.com>
2022-04-06 16:11:23 -0500

    add onclickDownload handler so it can be call anywhere

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,7 +1,23 @@
 import { createClient } from '@supabase/supabase-js'
 import { browserEnv } from '@/env/browser'
+import fileDownload from 'js-file-download'
 
 export const supabase = createClient(
   browserEnv.NEXT_PUBLIC_SUPABASE_URL,
   browserEnv.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
+
+export const onClickDownload = (fileUrl: string) => {
+  supabase.storage
+    .from('cheil-post')
+    .download(fileUrl)
+    .then((res) => {
+      console.log('res', fileUrl)
+      const hyphenIndex = fileUrl.indexOf('-')
+      const filename = fileUrl.substring(hyphenIndex + 1)
+      fileDownload(res.data!, filename)
+    })
+    .catch((e) => {
+      console.log('some error on supabase', e.message)
+    })
+}
